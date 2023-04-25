509509b13e559ca0ad44751efb8f35c6020f41b8
Andrew McKnight <andrewmcknight@J4PCG664JX.local>
2023-04-25 14:38:16 -0800

    fix bad old path to sentry_repos.el after merge

--- init.el
+++ init.el
@@ -2,16 +2,15 @@
 (load-file "~/.emacs.d/packages.el")
 (load-file "~/.emacs.d/keybindings.el")
 (load-file "~/.emacs.d/file_editing.el")
-(load-file "~/.emacs.d/sentry-repos.el")
 
 (load-file "~/.emacs.d/functions/config.el")
 (load-file "~/.emacs.d/functions/display.el")
 (load-file "~/.emacs.d/functions/emacs.el")
 (load-file "~/.emacs.d/functions/git.el")
 (load-file "~/.emacs.d/functions/makefile.el")
-(load-file "~/.emacs.d/sentry_repos.el")
+(load-file "~/.emacs.d/functions/sentry_repos.el")
 (load-file "~/.emacs.d/functions/snippets.el")
-(load-file "~/.emacs.d/functions/text_transformations.el")
+(load-file "~/.emacs.d/functions/text_transformation.el")
 
 ; use different file for Custom injections instead of this one
 (setq custom-file "~/.emacs.d/custom.el")
